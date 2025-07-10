8e04676c5c6e1d3c9b50566870b4c3380e767c10
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-10 15:16:59 -0300

    moved message handling code to separate file

---
+++ app/handler.py
@@ -0,0 +1,70 @@
+import logging
+from typing import Any
+
+from .database import get_value, set_value
+from .redis import REDIS_SEPARATOR, decode_redis, encode_redis
+
+REDIS_CONFIG = {}
+REDIS_QUIT = REDIS_SEPARATOR + REDIS_SEPARATOR
+
+
+def parse_redis(message: str) -> tuple[Any, int]:
+    logging.debug("new parse %s", repr(message))
+    return decode_redis(message)
+
+
+def handle_redis(recv_message: str) -> tuple[int, str]:
+    command_line, parsed_length = parse_redis(recv_message)
+    logging.debug(
+        "buffer %d %d %s",
+        parsed_length,
+        len(recv_message),
+        recv_message,
+    )
+    logging.debug("new buffer %d %d %s", 0, len(recv_message), recv_message)
+
+    logging.info(
+        "Command line %s (%d)",
+        str(command_line),
+        parsed_length,
+    )
+    command = command_line[0].upper()
+    arguments = command_line[1:] if len(command_line) > 1 else []
+
+    send_message = ""
+    match command:
+        case "QUIT":
+            send_message = REDIS_SEPARATOR
+        case "PING":
+            if arguments:
+                if len(arguments) == 1:
+                    send_message = encode_redis(arguments[0])
+                else:
+                    send_message = "-ERR wrong number of arguments for 'ping' command"
+            else:
+                send_message = "+PONG"
+        case "ECHO":
+            if len(arguments) == 1:
+                send_message = encode_redis(arguments[0])
+            else:
+                send_message = "-ERR wrong number of arguments for 'echo' command"
+        case "SET":
+            if len(arguments) < 2:
+                send_message = "-ERR wrong number of arguments for 'set' command"
+            else:
+                res = set_value(
+                    arguments[0],
+                    arguments[1],
+                    [arg.upper() for arg in arguments[2:]],
+                )
+                send_message = encode_redis(res)
+        case "GET":
+            if len(arguments) != 1:
+                send_message = "-ERR wrong number of arguments for 'get' command"
+            else:
+                res = get_value(arguments[0])
+                send_message = encode_redis(res)
+        case _:
+            logging.info("unhandled command %s", command)
+
+    return parsed_length, send_message + REDIS_SEPARATOR
--- app/server.py
+++ app/server.py
@@ -1,18 +1,11 @@
 import asyncio
 import logging
-from typing import Any
 
-from .database import get_value, set_value
-from .redis import REDIS_SEPARATOR, decode_redis, encode_redis
+from .handler import REDIS_CONFIG, REDIS_QUIT, handle_redis
+from .redis import REDIS_SEPARATOR
 
 REDIS_HOST = "localhost"
 REDIS_PORT = 6379
-REDIS_CONFIG = {}
-
-
-async def parse_redis(message: str) -> tuple[Any, int]:
-    logging.debug("new parse %s", repr(message))
-    return decode_redis(message)
 
 
 async def client_connected_cb(
@@ -29,70 +22,14 @@ async def client_connected_cb(
 
         if len(recv_message) > 0:
             logging.info("[%s] Recv %s", str(addr), repr(recv_message))
-            command_line, parsed_length = await parse_redis(recv_message)
-            logging.debug(
-                "buffer %d %d %s",
-                parsed_length,
-                len(recv_message),
-                recv_message,
-            )
+            parsed_length, send_message = handle_redis(recv_message)
             recv_message = recv_message[parsed_length:]
-            logging.debug("new buffer %d %d %s", 0, len(recv_message), recv_message)
 
-            logging.info(
-                "[%s] Command line %s (%d)",
-                str(addr),
-                str(command_line),
-                parsed_length,
-            )
-            command = command_line[0].upper()
-            arguments = command_line[1:] if len(command_line) > 0 else []
+            if send_message == REDIS_SEPARATOR:
+                continue
+            if send_message == REDIS_QUIT:
+                break
 
-            send_message = ""
-            match command:
-                case "QUIT":
-                    break
-                case "PING":
-                    if arguments:
-                        if len(arguments) == 1:
-                            send_message = encode_redis(arguments[0])
-                        else:
-                            send_message = (
-                                "-ERR wrong number of arguments for 'ping' command"
-                            )
-                    else:
-                        send_message = "+PONG"
-                case "ECHO":
-                    if len(arguments) == 1:
-                        send_message = encode_redis(arguments[0])
-                    else:
-                        send_message = (
-                            "-ERR wrong number of arguments for 'echo' command"
-                        )
-                case "SET":
-                    if len(arguments) < 2:
-                        send_message = (
-                            "-ERR wrong number of arguments for 'set' command"
-                        )
-                    else:
-                        res = set_value(
-                            arguments[0],
-                            arguments[1],
-                            [arg.upper() for arg in arguments[2:]],
-                        )
-                        send_message = encode_redis(res)
-                case "GET":
-                    if len(arguments) != 1:
-                        send_message = (
-                            "-ERR wrong number of arguments for 'get' command"
-                        )
-                    else:
-                        res = get_value(arguments[0])
-                        send_message = encode_redis(res)
-                case _:
-                    logging.info("unhandled command %s", command)
-
-            send_message += REDIS_SEPARATOR
             logging.info("[%s] Send %s", str(addr), repr(send_message))
             writer.write(send_message.encode())
             await writer.drain()
