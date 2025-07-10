37a7386c025bc24a42faacc0bbe2e0fe0a4bdac7
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-10 15:47:02 -0300

    handle config get and set (single parameter)

--- app/handler.py
+++ app/handler.py
@@ -58,6 +58,35 @@ def handle_redis(recv_message: str) -> tuple[int, str]:
             else:
                 res = get_value(arguments[0])
                 send_message = encode_redis(res)
+        case "CONFIG":
+            if len(arguments) < 1:
+                send_message = "-ERR missing arguments for 'CONFIG' command"
+            else:
+                option = arguments[0].upper()
+                if option == "GET":
+                    if len(arguments) < 2:
+                        send_message = (
+                            "-ERR missing parameters for 'CONFIG GET' command"
+                        )
+                    else:
+                        name = arguments[1]
+                        if name in REDIS_CONFIG:
+                            value = REDIS_CONFIG[name]
+                            send_message = encode_redis([name, value])
+                        else:
+                            send_message = "-ERR unknown 'CONFIG' parameter"
+                elif option == "SET":
+                    if len(arguments) < 3:
+                        send_message = (
+                            "-ERR missing parameters for 'CONFIG SET' command"
+                        )
+                    else:
+                        name = arguments[1]
+                        value = arguments[2]
+                        REDIS_CONFIG[name] = value
+                        send_message = "OK"
+                else:
+                    send_message = "-ERR unhandled 'CONFIG' option"
         case _:
             logging.info("unhandled command %s", command)
 
