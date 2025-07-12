e1bb5adeb687870baff9cd4e5c1461d2542f0430
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-11 23:05:27 -0300

    added RDB checksum codec

---
+++ app/rdb/file/checksum.py
@@ -0,0 +1,18 @@
+import struct
+
+from ..constants import RDBOpCode
+
+
+def read_rdb_checksum(buffer: bytes, pos: int = 0) -> tuple[int, int]:
+    if len(buffer[pos:]) < 9:
+        return pos, 0
+
+    if buffer[pos] != RDBOpCode.EOF:
+        return pos, 0
+
+    checksum = struct.unpack("<Q", buffer[pos + 1 : pos + 9])[0]
+    return pos + 9, checksum
+
+
+def save_rdb_checksum(checksum: int) -> bytes:
+    return bytes([RDBOpCode.EOF]) + struct.pack("<Q", checksum)
