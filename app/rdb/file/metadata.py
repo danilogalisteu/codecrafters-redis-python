3dbcbb25355425cd6e097080207d54c1cd002244
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-11 23:03:18 -0300

    added RDB metadata codec

---
+++ app/rdb/file/metadata.py
@@ -0,0 +1,27 @@
+from ..constants import RDBOpCode
+from ..string import decode_string, encode_string
+
+
+def read_rdb_meta(buffer: bytes, pos: int = 0) -> tuple[int, dict[str, str]]:
+    data = {}
+    while True:
+        if buffer[pos] != RDBOpCode.AUX:
+            return pos, data
+        pos += 1
+        skey, vkey = decode_string(buffer[pos:])
+        if skey == 0:
+            raise ValueError("error reading RDB AUX name")
+        pos += skey
+        sval, vval = decode_string(buffer[pos:])
+        if sval == 0:
+            raise ValueError("error reading RDB AUX value")
+        pos += sval
+        data[vkey] = vval
+
+
+def save_rdb_meta(meta: dict[str, str]) -> bytes:
+    buffer = bytes()
+    for key, value in meta.items():
+        buffer += encode_string(key)
+        buffer += encode_string(value)
+    return buffer
