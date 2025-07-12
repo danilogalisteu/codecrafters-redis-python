ae31ccd63fbdee0d3cad306cc169853b351373e4
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-11 23:03:02 -0300

    added RDB header codec

---
+++ app/rdb/file/header.py
@@ -0,0 +1,10 @@
+from ..constants import RDB_NAME, RDB_VERSION
+
+
+def read_rdb_header(buffer: bytes) -> tuple[int, str]:
+    assert buffer[:5].decode() == RDB_NAME
+    return 9, buffer[5:9].decode()
+
+
+def save_rdb_header(version: str = RDB_VERSION) -> bytes:
+    return RDB_NAME.encode() + version.encode()
