0bc92c49e8d972887b6909e4ae0ff52148c05a7b
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-14 23:18:56 -0300

    changed print to logging

--- app/redis/handshake.py
+++ app/redis/handshake.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 
 from .database import read_db
 from .rdb.data import decode_data
@@ -9,48 +10,48 @@ async def send_handshake(
     reader: asyncio.StreamReader, writer: asyncio.StreamWriter, slave_port: int
 ) -> tuple[str, int]:
     message = encode_redis(["PING"]) + REDIS_SEPARATOR
-    print(f"Sending: {message!r}")
+    logging.info("Sending %s", repr(message))
     writer.write(message.encode())
     await writer.drain()
 
     data = await reader.read()
-    print(f"Received: {data.decode()!r}")
+    logging.info("Received %s", repr(data.decode()))
     assert data.decode().removesuffix(REDIS_SEPARATOR) == "+PONG"
 
     message = (
         encode_redis(["REPLCONF", "listening-port", str(slave_port)]) + REDIS_SEPARATOR
     )
-    print(f"Sending: {message!r}")
+    logging.info("Sending %s", repr(message))
     writer.write(message.encode())
     await writer.drain()
 
     data = await reader.read()
-    print(f"Received: {data.decode()!r}")
+    logging.info("Received %s", repr(data.decode()))
     assert data.decode().removesuffix(REDIS_SEPARATOR) == "+OK"
 
     message = encode_redis(["REPLCONF", "capa", "psync2"]) + REDIS_SEPARATOR
-    print(f"Sending: {message!r}")
+    logging.info("Sending %s", repr(message))
     writer.write(message.encode())
     await writer.drain()
 
     data = await reader.read()
-    print(f"Received: {data.decode()!r}")
+    logging.info("Received %s", repr(data.decode()))
     assert data.decode().removesuffix(REDIS_SEPARATOR) == "+OK"
 
     message = encode_redis(["PSYNC", "?", "-1"]) + REDIS_SEPARATOR
-    print(f"Sending: {message!r}")
+    logging.info("Sending %s", repr(message))
     writer.write(message.encode())
     await writer.drain()
 
     data = await reader.read()
-    print(f"Received: {data.decode()!r}")
+    logging.info("Received %s", repr(data.decode()))
 
     recv_message = data.decode().removesuffix(REDIS_SEPARATOR).split(" ")
     assert recv_message[0] == "+FULLRESYNC"
     master_id, master_offset = recv_message[1], int(recv_message[2])
 
     data = await reader.read()
-    print(f"Received: {data!r}")
+    logging.info("Received %s", repr(data))
 
     read_db(decode_data(data))
 
