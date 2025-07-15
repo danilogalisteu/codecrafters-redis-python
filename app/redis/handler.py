473a64df0d3e23f43f22456de79fb8a7ee6cfa11
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-15 13:45:23 -0300

    fix data type

--- app/redis/handler.py
+++ app/redis/handler.py
@@ -110,7 +110,7 @@ def handle_redis(recv_message: str) -> tuple[int, str, bool, str]:
             if len(arguments) == 0:
                 send_message = "+OK"
             elif arguments[0].upper() == "GETACK":
-                send_replica = encode_redis(["REPLCONF", "ACK", 0]) + REDIS_SEPARATOR
+                send_replica = encode_redis(["REPLCONF", "ACK", "0"]) + REDIS_SEPARATOR
         case "PSYNC":
             repl_id = get_info("replication", "master_replid")
             if repl_id == "":
