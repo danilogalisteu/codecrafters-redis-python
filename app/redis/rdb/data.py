72e57e5e201caae8a17b1e0b84b8ddc7db472a66
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-15 10:43:36 -0300

    removed logging

--- app/redis/rdb/data.py
+++ app/redis/rdb/data.py
@@ -8,20 +8,11 @@ def decode_data(buffer: bytes) -> tuple[bytes, int]:
     if len(buffer) < 1:
         return b"", 0
     if chr(buffer[0]) != IDAggregate.BSTRING:
-        logging.info("not expected")
         return b"", 0
 
     rdb_length_end = buffer.find(REDIS_SEPARATOR.encode())
     rdb_length = int(buffer[1:rdb_length_end])
     rdb_data_start = rdb_length_end + len(REDIS_SEPARATOR.encode())
-    logging.info(
-        "%d %d %d %d %d",
-        len(buffer),
-        rdb_length_end,
-        rdb_length,
-        rdb_data_start,
-        rdb_data_start + rdb_length,
-    )
     if len(buffer) < rdb_data_start + rdb_length:
         return b"", 0
     return buffer[rdb_data_start : rdb_data_start + rdb_length], rdb_data_start + rdb_length
