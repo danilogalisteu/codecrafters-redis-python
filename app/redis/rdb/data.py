c835a29ba54b048002e91e6e828461e33d4705b9
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-15 10:40:10 -0300

    fix parsing extra data

--- app/redis/rdb/data.py
+++ app/redis/rdb/data.py
@@ -3,21 +3,28 @@ import logging
 from app.redis.resp import REDIS_SEPARATOR, IDAggregate
 
 
-def decode_data(buffer: bytes) -> bytes:
+def decode_data(buffer: bytes) -> tuple[bytes, int]:
     logging.info("buffer %d %s", len(buffer), repr(buffer))
     if len(buffer) < 1:
-        return b""
+        return b"", 0
     if chr(buffer[0]) != IDAggregate.BSTRING:
         logging.info("not expected")
-        return b""
+        return b"", 0
 
     rdb_length_end = buffer.find(REDIS_SEPARATOR.encode())
     rdb_length = int(buffer[1:rdb_length_end])
     rdb_data_start = rdb_length_end + len(REDIS_SEPARATOR.encode())
-    logging.info("%d %d %d %d %d", len(buffer), rdb_length_end, rdb_length, rdb_data_start, rdb_data_start + rdb_length)
+    logging.info(
+        "%d %d %d %d %d",
+        len(buffer),
+        rdb_length_end,
+        rdb_length,
+        rdb_data_start,
+        rdb_data_start + rdb_length,
+    )
     if len(buffer) < rdb_data_start + rdb_length:
-        return b""
-    return buffer[rdb_data_start:]
+        return b"", 0
+    return buffer[rdb_data_start : rdb_data_start + rdb_length], rdb_data_start + rdb_length
 
 
 def encode_data(data: bytes) -> bytes:
