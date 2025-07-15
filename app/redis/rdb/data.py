0cf4fcf3a573ed4ec48c56e5d864d59ac31e0d42
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-15 00:10:03 -0300

    added log

--- app/redis/rdb/data.py
+++ app/redis/rdb/data.py
@@ -1,3 +1,5 @@
+import logging
+
 from app.redis.resp import REDIS_SEPARATOR, IDAggregate
 
 
@@ -10,7 +12,7 @@ def decode_data(buffer: bytes) -> bytes:
     rdb_length_end = buffer.find(REDIS_SEPARATOR.encode())
     rdb_length = int(buffer[1:rdb_length_end])
     rdb_data_start = rdb_length_end + len(REDIS_SEPARATOR.encode())
-    print(len(buffer), rdb_length_end, rdb_length, rdb_data_start, rdb_data_start + rdb_length)
+    logging.info("%d %d %d %d %d", len(buffer), rdb_length_end, rdb_length, rdb_data_start, rdb_data_start + rdb_length)
     if len(buffer) < rdb_data_start + rdb_length:
         return b""
     return buffer[rdb_data_start:]
