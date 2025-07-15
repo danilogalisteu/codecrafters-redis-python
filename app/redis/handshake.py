c0ada2034e7eadfb145cccc886250f9de1efe8bc
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-14 22:34:01 -0300

    renamed file

--- app/redis/handshake.py
+++ app/redis/handshake.py
@@ -3,16 +3,17 @@ import asyncio
 from .resp import REDIS_SEPARATOR, encode_redis
 
 
-async def send_handshake(master_host: str, master_port: int, slave_port: int) -> None:
-    reader, writer = await asyncio.open_connection(master_host, master_port)
-
+async def send_handshake(
+    reader: asyncio.StreamReader, writer: asyncio.StreamWriter, slave_port: int
+) -> tuple[str, int]:
     message = encode_redis(["PING"]) + REDIS_SEPARATOR
     print(f"Sending: {message!r}")
     writer.write(message.encode())
     await writer.drain()
 
-    data = await reader.read(100)
+    data = await reader.read()
     print(f"Received: {data.decode()!r}")
+    assert data.decode().removesuffix(REDIS_SEPARATOR) == "+PONG"
 
     message = (
         encode_redis(["REPLCONF", "listening-port", str(slave_port)]) + REDIS_SEPARATOR
@@ -21,25 +22,28 @@ async def send_handshake(master_host: str, master_port: int, slave_port: int) ->
     writer.write(message.encode())
     await writer.drain()
 
-    data = await reader.read(100)
+    data = await reader.read()
     print(f"Received: {data.decode()!r}")
+    assert data.decode().removesuffix(REDIS_SEPARATOR) == "+OK"
 
     message = encode_redis(["REPLCONF", "capa", "psync2"]) + REDIS_SEPARATOR
     print(f"Sending: {message!r}")
     writer.write(message.encode())
     await writer.drain()
 
-    data = await reader.read(100)
+    data = await reader.read()
     print(f"Received: {data.decode()!r}")
+    assert data.decode().removesuffix(REDIS_SEPARATOR) == "+OK"
 
     message = encode_redis(["PSYNC", "?", "-1"]) + REDIS_SEPARATOR
     print(f"Sending: {message!r}")
     writer.write(message.encode())
     await writer.drain()
 
-    data = await reader.read(100)
+    data = await reader.read()
     print(f"Received: {data!r}")
 
-    print("Close the connection")
-    writer.close()
-    await writer.wait_closed()
+    recv_message = data.decode().removesuffix(REDIS_SEPARATOR).split(" ")
+    assert recv_message[0] == "+FULLRESYNC"
+
+    return recv_message[1], int(recv_message[2])
