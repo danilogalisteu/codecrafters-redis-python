9d584ea440563efa230bf96e017539f40cfa2a63
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-15 15:48:15 -0300

    code to wait for slaves with timeout

--- app/redis/slave.py
+++ app/redis/slave.py
@@ -13,10 +13,6 @@ async def init_slave(writer: asyncio.StreamWriter) -> None:
     REDIS_SLAVES.append(writer)
 
 
-def get_num_slaves() -> int:
-    return len(REDIS_SLAVES)
-
-
 async def send_write(send_message: str) -> None:
     logging.info("Replicating message %s", repr(send_message))
     closed = []
@@ -31,3 +27,16 @@ async def send_write(send_message: str) -> None:
             REDIS_SLAVES.remove(writer)
         except ValueError:
             pass
+
+
+async def wait_slaves(num_slaves: int, timeout_ms: int) -> int:
+    async def async_wait(num: int) -> None:
+        while len(REDIS_SLAVES) < num:
+            await asyncio.sleep(0)
+
+    await asyncio.wait_for(
+        async_wait(num_slaves),
+        float(timeout_ms) / 1000,
+    )
+
+    return len(REDIS_SLAVES)
