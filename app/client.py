eb87d6cbf29f1741423b4da5cfbd48a2fd1ca7f1
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-15 13:45:47 -0300

    don't parse if empty

--- app/client.py
+++ app/client.py
@@ -17,7 +17,8 @@ async def run_client(master_host: str, master_port: int, slave_port: int) -> Non
     while True:
         if len(recv_message) > 0:
             logging.info("Master recv  %s", repr(recv_message))
-            while True:
+            send_message = ""
+            while len(recv_message) > 0:
                 parsed_length, send_message, _, send_replica = handle_redis(recv_message)
                 recv_message = recv_message[parsed_length:]
                 if send_replica:
