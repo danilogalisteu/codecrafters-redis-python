4f15bc53cd2cf8c76384e8f997697ba7046b02ef
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-16 12:00:43 -0300

    formatting

--- app/client.py
+++ app/client.py
@@ -24,11 +24,13 @@ async def run_client(master_host: str, master_port: int, slave_port: int) -> Non
                     recv_message, master_offset
                 )
                 recv_message = recv_message[parsed_length:]
+                master_offset += parsed_length
+
                 if send_master:
                     logging.info("Master send %s", repr(send_master))
                     writer.write(send_master.encode())
                     await writer.drain()
-                master_offset += parsed_length
+
                 if (parsed_length == 0) or (send_message == REDIS_QUIT):
                     break
 
