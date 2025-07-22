c10e644eb74e80e2c8ed5f2f1da37ce9719b9a4c
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-22 10:46:09 -0300

    export decode_redis

--- app/redis/__init__.py
+++ app/redis/__init__.py
@@ -1,10 +1,12 @@
 from .handler import REDIS_QUIT, REDIS_SEPARATOR, handle_redis
+from .resp import decode_redis
 from .setup import setup_redis
 from .slave import init_slave, send_write
 
 __all__ = [
     "REDIS_QUIT",
     "REDIS_SEPARATOR",
+    "decode_redis",
     "handle_redis",
     "init_slave",
     "send_write",
