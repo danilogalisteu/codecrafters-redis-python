c7228d545d3f57f602e03ae07e8010670baafacf
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-22 20:59:06 -0300

    added code to get list values

--- app/redis/database/__init__.py
+++ app/redis/database/__init__.py
@@ -1,10 +1,11 @@
 from .data import get_keys, get_type, load_db, read_db, save_db, write_db
-from .list import push_list_value
+from .list import get_list_values, push_list_value
 from .stream import get_stream_range, get_stream_values, set_stream_value
 from .value import get_value, increase_value, set_value
 
 __all__ = [
     "get_keys",
+    "get_list_values",
     "get_stream_range",
     "get_stream_values",
     "get_type",
--- app/redis/database/list.py
+++ app/redis/database/list.py
@@ -5,6 +5,15 @@ from app.redis.rdb.file.constants import DBType
 from .data import check_key, get_data, set_data
 
 
+def get_list_values(key: str, start: int, end: int) -> list[str]:
+    logging.info("LRANGE key '%s' start %d end %d", key, start, end)
+    if not check_key(key):
+        return []
+
+    vlist, _ = get_data(key)
+    return vlist["value"][start : end + 1]
+
+
 def push_list_value(key: str, values: list[str]) -> int:
     logging.info("RPUSH key '%s' values %s", key, values)
     if not check_key(key):
