4f8b03e9ffea064469204d5416b2628cfe5df530
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-22 21:25:48 -0300

    added code for list length

--- app/redis/database/list.py
+++ app/redis/database/list.py
@@ -5,6 +5,15 @@ from app.redis.rdb.file.constants import DBType
 from .data import check_key, get_data, set_data
 
 
+def get_list_length(key: str) -> int:
+    logging.info("LLEN key '%s'", key)
+    if not check_key(key):
+        return 0
+
+    vlist, _ = get_data(key)
+    return len(vlist["value"])
+
+
 def get_list_values(key: str, start: int, end: int) -> list[str]:
     logging.info("LRANGE key '%s' start %d end %d", key, start, end)
     if not check_key(key):
