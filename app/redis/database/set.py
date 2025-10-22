7fcb22cba648242925de086b5c361b975b4901b0
Danilo Galisteu <danilogalisteu@gmail.com>
2025-10-22 14:06:07 -0300

    sort same-score members alphabetically

--- app/redis/database/set.py
+++ app/redis/database/set.py
@@ -8,13 +8,13 @@ from .data import check_key, get_data, set_data
 def set_set_value(key: str, values: dict[str, float]) -> bytes:
     logging.info("ZADD key '%s' values %s", key, values)
     if not check_key(key):
-        sorted_values = list(sorted(values.items(), key=lambda x: x[1]))
+        sorted_values = list(sorted(sorted(values.items(), key=lambda x: x[0]), key=lambda x: x[1]))
         set_data(key, sorted_values, dtype=DBType.ZSET)
         return len(sorted_values)
 
     vzset, _ = get_data(key)
     vdict = dict(vzset["value"])
     vdict.update(values)
-    sorted_values = list(sorted(vdict.items(), key=lambda x: x[1]))
+    sorted_values = list(sorted(sorted(vdict.items(), key=lambda x: x[0]), key=lambda x: x[1]))
     set_data(key, sorted_values, dtype=DBType.ZSET)
     return len(sorted_values) - len(vzset["value"])
